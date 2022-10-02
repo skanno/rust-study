9a62dd6b547fb6e0c0a481c05f084a591b4de81f
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-10-02 08:12:19 +0000

    5.3を実装。

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,13 +4,35 @@ struct Rectangle {
     height: u32,
 }
 
-fn main() {
-    let rect1 = Rectangle {width: 30, height: 50};
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 
-    println!("rect1 is {:?}", rect1);
-    println!("Thi area of rectangle is {} square pixels.", area(&rect1));
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+fn main() {
+    let rect1 = Rectangle {width: 30, height: 50};
+    let rect2 = Rectangle {width: 10, height: 40};
+    let rect3 = Rectangle {width: 60, height: 45};
+
+    println!("rect1 is {:?}", rect1);
+    println!("Thi area of rectangle is {} square pixels.", rect1.area());
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let rect4 = Rectangle::square(3);
+    println!("rect4 is {:?}", rect4);
+    println!("Thi area of rect4 is {} square pixels.", rect4.area());
 }
