7173544132b1c2103be224a9a4102485a46fb850
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-10-02 07:44:38 +0000

    5.2を実装。

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,9 +1,16 @@
-fn main() {
-    let rect1 = (30, 50);
-
-    println!("Thi area of rectangle is {} square pixels.", area(rect1));
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn main() {
+    let rect1 = Rectangle {width: 30, height: 50};
+
+    println!("rect1 is {:?}", rect1);
+    println!("Thi area of rectangle is {} square pixels.", area(&rect1));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
