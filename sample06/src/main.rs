3a9bc319e15eea735245c15360db5c677cfd598c
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-10-06 09:25:14 +0000

    追加実装。

--- sample06/src/main.rs
+++ sample06/src/main.rs
@@ -4,9 +4,26 @@ enum IpAddrKind {
     V6,
 }
 
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
 fn main() {
     let four = IpAddrKind::V4;
     let six = IpAddrKind::V6;
     println!("{:?}", four);
     println!("{:?}", six);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+    println!("{:?}", home);
+    println!("{:?}", loopback);
 }
