5a48afa9e26bb3ec6aaeaff2524c20ee576d1143
Shoichiro KANNO <s-kanno@qb3.so-net.ne.jp>
2022-10-06 09:58:27 +0000

    追加実装。

--- sample06/src/main.rs
+++ sample06/src/main.rs
@@ -4,10 +4,27 @@ enum IpAddrKind {
     V6(String),
 }
 
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColer(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", self);
+    }
+}
 
 fn main() {
     let home = IpAddrKind::V4(127, 0, 0, 1);
     let loopback = IpAddrKind::V6(String::from("::1"));
     println!("{:?}", home);
     println!("{:?}", loopback);
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
+
